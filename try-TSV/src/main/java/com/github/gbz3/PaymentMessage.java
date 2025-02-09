a751258d89e182d9f4d03fe7b21cde00cac314f4
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-02-09 10:07:32 +0900

    2nd commit

--- try-TSV/src/main/java/com/github/gbz3/PaymentMessage.java
+++ try-TSV/src/main/java/com/github/gbz3/PaymentMessage.java
@@ -1,23 +1,28 @@
 package com.github.gbz3;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class PaymentMessage {
     private final int _mti;
+    private final int _transactionCode;
 
-    private PaymentMessage(int mti) {
+    private PaymentMessage(int mti, int transactionCode) {
         this._mti = mti;
+        this._transactionCode = transactionCode;
     }
 
-    @Contract("_ -> new")
-    public static @NotNull PaymentMessage of(int mti) {
-        if (mti < 0 || 9999 < mti) throw new IllegalArgumentException();
-        return new PaymentMessage(mti);
+    public static @NotNull PaymentMessage of(int mti, int transactionCode) {
+        if (mti < 0 || 9999 < mti) throw new IllegalArgumentException("Invalid mti");
+        if (transactionCode < 0 || 999 < transactionCode) throw new IllegalArgumentException("Invalid transaction code");
+        return new PaymentMessage(mti, transactionCode);
     }
 
     public String getMTI() {
         return String.format("%04d", _mti);
+    }
+
+    public String getTransactionCode() {
+        return String.format("%03d", _transactionCode);
     }
 
 }
--- try-TSV/src/test/java/com/github/gbz3/PaymentMessageTest.java
+++ try-TSV/src/test/java/com/github/gbz3/PaymentMessageTest.java
@@ -6,12 +6,35 @@ import org.assertj.core.api.Assertions;
 
 public class PaymentMessageTest {
 
+    @Example
+    void 例外がスローされることを確認する() {
+        Assertions.assertThatThrownBy(() -> PaymentMessage.of(-1, 999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid mti");
+
+        Assertions.assertThatThrownBy(() -> PaymentMessage.of(10000, 999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid mti");
+
+        Assertions.assertThatThrownBy(() -> PaymentMessage.of(9999, -1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid transaction code");
+
+        Assertions.assertThatThrownBy(() -> PaymentMessage.of(9999, 1000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid transaction code");
+    }
+
     @Property
-    void indicatorOfMessageIs4Number(
-            @ForAll @IntRange(min = 0, max = 9999) int mti
+    void MTIは4桁の数字かつTransactionCodeは３桁の数字(
+            @ForAll @IntRange(max = 9999) int mti,
+            @ForAll @IntRange(max = 999) int transactionCode
     ) {
-        var message = PaymentMessage.of(mti);
+        var message = PaymentMessage.of(mti, transactionCode);
         Assertions.assertThat(message.getMTI()).containsOnlyDigits();
         Assertions.assertThat(message.getMTI().length()).isEqualTo(4);
+        Assertions.assertThat(message.getTransactionCode()).containsOnlyDigits();
+        Assertions.assertThat(message.getTransactionCode().length()).isEqualTo(3);
     }
+
 }
